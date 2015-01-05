1012452b4d94aaabeb41943e5f191a70048dcf5f
Dhaval Desai <dydesai@gmail.com>
2015-01-05 14:17:48 +0530

    CQ DLL Testing

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -14,6 +14,9 @@
  * iterator
  * size
  */
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class DoublyLinkedList<Item> implements Iterable<Item> {
 
 	private Node FIRST;
@@ -48,8 +51,9 @@ public class DoublyLinkedList<Item> implements Iterable<Item> {
 
 	public void insertLast(Item item) {
 		Node<Item> newNode = new Node<Item>();
+		newNode.item = item;
 		if ( FIRST == null ) {
-			newNode.next = = newNode.prev = null;
+			newNode.next = newNode.prev = null;
 			FIRST = LAST = newNode;
 		} else {
 			newNode.prev = LAST;
@@ -61,6 +65,7 @@ public class DoublyLinkedList<Item> implements Iterable<Item> {
 
 	public void insertAfter(Item item, Node node) {
 		Node<Item> newNode = new Node<Item>();
+		newNode.item = item;
 		if ( LAST == node ) {
 			newNode.prev = LAST;
 			newNode.next = null;
@@ -75,6 +80,7 @@ public class DoublyLinkedList<Item> implements Iterable<Item> {
 
 	public void insertBefore(Item item, Node node) {
 		Node<Item> newNode = new Node<Item>();
+		newNode.item = item;
 		if ( FIRST == node ) {
 			newNode.prev = LAST;
 			newNode.next = null;
@@ -84,6 +90,88 @@ public class DoublyLinkedList<Item> implements Iterable<Item> {
 			newNode.prev = node;
 			newNode.next = node.next;
 			node.next = newNode;
+		}
+	}
+
+	public Item remove(Node node) {
+		if ( node == LAST && node == FIRST ) {
+			FIRST = LAST = null;
+		} else if ( FIRST == node ) {
+			FIRST = FIRST.next;
+			FIRST.prev = null;
+		} else if ( LAST == node ) {
+			LAST = LAST.prev;
+			LAST.next = null;
+		} else {
+			node.prev.next = node.next;
+			node.next.prev = node.prev;
+			node.next = node.prev = null;
+		}		
+		Item item = (Item)node.item;
+		return item;
+	}
+
+	public boolean isEmpty() {
+		return FIRST == null;
+	}
+
+	public Iterator<Item> iterator() {
+		return new DoublyLinkedListIterator();
+	}
+
+	public class DoublyLinkedListIterator<Item> implements Iterator<Item> {
+		Node currentNode;
+
+		public DoublyLinkedListIterator(){
+			currentNode = FIRST;
+		}
+		public boolean hasNext() {
+			return currentNode.next == null;
+		}
+		public Item next(){
+			if(!hasNext()) {
+				currentNode = currentNode.next;
+				Item item = (Item) currentNode.item;
+				return item;
+			} else {
+				throw new NoSuchElementException();
+			}			
+		}
+		public boolean hasPrevious() {
+			return currentNode.prev == null;
+		}
+		public Item previous() {
+			if(hasPrevious()) {
+				currentNode = currentNode.prev;
+				Item item = (Item) currentNode.item;
+				return item;
+			} else {
+				throw new NoSuchElementException();
+			}			
+		}
+		public void remove(){
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	public static void main(String[] args){
+		DoublyLinkedList<Integer> dll = new DoublyLinkedList<Integer>();
+		for (int i=0; i< 20; i++) {
+			System.out.println(i);
+			dll.insertFirst(i);
+		}
+		
+		Iterator it = dll.iterator();
+		for (int i = 0; i < 30; i++ ) {
+			try {
+				System.out.println(it.next());
+			} catch ( Exception e ) {
+				System.out.println("Exception:" + i + "  " + e);
+			}
+		}
+
+		for (int i=0; i< 20; i++) {
+			System.out.println(dll.remove(dll.LAST));
 		}
 	}
 
