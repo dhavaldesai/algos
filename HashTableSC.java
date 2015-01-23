e94e27d5851b0ec81d1ece64dd61bebcf07a0980
Dhaval Desai <ddesai@quikr.com>
2015-01-23 17:25:30 +0530

    hashing

---
+++ HashTableSC.java
@@ -0,0 +1,21 @@
+/**
+ * HastTableSC.java - Hash table implementation using Separate Chaining
+ * get
+ * set
+ * hashcode
+ * isequal
+ * array of linked list
+ *
+ */
+public class HashTableSC<Key extends Comparable<Key>, Value> {
+
+	int N;
+
+
+	public HashTableSC(int capacity) {
+		N = capacity;
+	}
+
+	
+
+}
---
+++ SeparateChain.java
@@ -0,0 +1,9 @@
+/**
+ * SeparateChain.java - LinkedList for HashTable
+ * get
+ * set
+ * iterate
+ * size
+ * contains
+ * delete
+ */
